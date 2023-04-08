96b2f9f143e4a26bbdddf14d5747eed851e660d3
Akhil <akhilesh.tulluri@gmail.com>
2023-04-09 03:25:41 +0530

    feat(schedule): add a way to plan trip

---
+++ src/pages/api/poi.js
@@ -0,0 +1,34 @@
+const GPT_KEY = "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";
+
+const headers = {
+  "Content-Type": "application/json",
+  Authorization: `Bearer ${GPT_KEY}`,
+};
+
+export default async function handler(req, res) {
+  const { days, city } = req.query;
+
+  let basePrompt = `what is an ideal itinerary for ${days} days in ${city}?`;
+  try {
+    const response = await fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
+      headers,
+      body: JSON.stringify({
+        model: "text-davinci-003",
+        prompt: basePrompt,
+        temperature: 0,
+        max_tokens: 550,
+      }),
+    });
+    const itinerary = await response.json();
+    const pointsOfInterestPrompt =
+      "Extract the points of interest out of this text, with no additional words, separated by commas: " +
+      itinerary.choices[0].text;
+
+    const result = itinerary.choices[0].text.split("\n\n");
+    result.shift();
+    res.status(200).send({ schedule: result });
+  } catch (err) {
+    console.log("error: ", err);
+  }
+}
